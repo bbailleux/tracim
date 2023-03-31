9adf743feff0c71ba8113fc644f4203974bcdc41
Mathis Perrier <mathis.perrier@algoo.fr>
2023-03-31 11:15:34 +0200

    fix(backend): lint

--- backend/tracim_backend/views/core_api/schemas.py
+++ backend/tracim_backend/views/core_api/schemas.py
@@ -1515,9 +1515,6 @@ class WorkspaceMemberSchema(WorkspaceMemberDigestSchema):
 
 
 class WorkspaceWithUserMemberSchema(WorkspaceSchema):
-    """Workspace as returned by the user's workspace API.
-    Includes the list of members within it.
-    """
     members = marshmallow.fields.Nested(WorkspaceMemberSchema(many=True))
 
 
